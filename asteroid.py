e9b02eca9d8f689d72498bbf34c01394b65fdb5e
tapsaca <98756983+tapsaca@users.noreply.github.com>
2026-01-13 23:07:43 +0200

    Override draw in Asteroid

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,10 @@
+import pygame
 from circleshape import CircleShape
+from constants import LINE_WIDTH
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         CircleShape.__init__(self, x, y, radius)
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
