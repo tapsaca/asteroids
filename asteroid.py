a003b5f0d80599676b1a8fdfa9cfa8cf1fbcef31
tapsaca <98756983+tapsaca@users.noreply.github.com>
2026-01-14 16:18:08 +0200

    Override update in Asteroid

--- asteroid.py
+++ asteroid.py
@@ -8,3 +8,6 @@ class Asteroid(CircleShape):
     
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+    
+    def update(self, dt):
+        self.position += self.velocity * dt
